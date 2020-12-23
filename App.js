ebed0e6446e9f6c535248d25d32a98b4179a28ef
Dekpo Yologaza <dekpo@me.com>
2020-12-23 17:03:46 +0100

    Added all the necessary stuff...

--- App.js
+++ App.js
@@ -1,21 +1,113 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Home = ({navigation}) => {
+  return(
+    <View style={styles.container}>
+    <Text>HOME</Text>
+    <Button title="Got to Detail"
+    onPress={() => { navigation.navigate('Detail',{
+      id: 544, param2: 'coucou machin'
+    }
+    )}} 
+    />
+  </View>
+  )
+}
+
+const Detail = ({route, navigation}) => {
+
+  const { id, param2 } = route.params;
+
+  return(
+    <View style={styles.container}>
+
+    <Text>DETAIL</Text>
+    <Text>{ id }</Text>
+    <Text>{ param2 }</Text>
+    <Button title="Change Options"
+    onPress={() => { navigation.setOptions({title: 'Title Changed'})}} 
+    />
+
+    <Button title="Change params"
+    onPress={() => { navigation.setParams({id: 123, param2: 'YO'})}} 
+    />
+
+    <Button title="Got to Home"
+    onPress={() => { navigation.navigate('Home')}} 
+    />
+    <Button title="Got to another Detail page"
+    onPress={() => { navigation.push('Detail',
+    {
+      id: Math.round(Math.random() * 1000)
+    }
+    )}} 
+    />
+    <Button title="Got Back"
+    onPress={() => { navigation.goBack()}} 
+    />
+     <Button title="Got to first screen"
+    onPress={() => { navigation.popToTop()}} 
+    />
+  </View>
+  )
+}
+
+const TopRight = () => {
+  return(
+  <Button
+  onPress={()=>{
+    alert('Yo HE IS ALIVE !!!')
+  }} 
+  title="Info" />
+  )
+}
+
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} 
+      options={{title: 'My Title',
+
+      headerStyle: styles.headerScreen,
+      headerTintColor: '#000',
+      headerTitleStyle:{
+        fontWeight: 'bold'
+      },
+      headerRight: () => TopRight()
+      
+      }} />
+      <Stack.Screen name="Detail" component={Detail}
+      options={
+        ({ route }) => ({
+          title: 'Detail ' + route.params.id + ' ' + route.params.param2
+        }),{
+      headerStyle: styles.headerScreen
+        }  
+      }
+      initialParams={{ id: 1234 }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  headerScreen: {
+    backgroundColor: '#EEEEEE',
+    borderBottomColor: 'black',
+    borderBottomWidth: 1
+  }
 });
---
+++ README.md
@@ -0,0 +1,7 @@
+# myReactNavStack
+
+This is a React Native App
+
+run expo install to get all dependencies 
+
+run expo start to launch server on the browser...
